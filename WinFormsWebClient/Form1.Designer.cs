c9d9c197447d9ae5e426f60df99d1a8fbd756439
Alla Ponurko <aponurko@gmail.com>
2022-10-14 23:56:18 +0300

    Добавьте файлы проекта.

---
+++ WinFormsWebClient/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace WinFormsWebClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtRequerst = new System.Windows.Forms.TextBox();
+            this.btnSync = new System.Windows.Forms.Button();
+            this.btnAsync = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(13, 13);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(269, 23);
+            this.txtUrl.TabIndex = 0;
+            this.txtUrl.Text = "https://www.ninydev.com/robots.txt\r\n";
+            // 
+            // txtRequerst
+            // 
+            this.txtRequerst.Location = new System.Drawing.Point(13, 51);
+            this.txtRequerst.Multiline = true;
+            this.txtRequerst.Name = "txtRequerst";
+            this.txtRequerst.Size = new System.Drawing.Size(269, 279);
+            this.txtRequerst.TabIndex = 1;
+            // 
+            // btnSync
+            // 
+            this.btnSync.Location = new System.Drawing.Point(309, 13);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(75, 23);
+            this.btnSync.TabIndex = 2;
+            this.btnSync.Text = "GetSync";
+            this.btnSync.UseVisualStyleBackColor = true;
+            this.btnSync.Click += new System.EventHandler(this.btnSinc_Click);
+            // 
+            // btnAsync
+            // 
+            this.btnAsync.Location = new System.Drawing.Point(309, 51);
+            this.btnAsync.Name = "btnAsync";
+            this.btnAsync.Size = new System.Drawing.Size(75, 23);
+            this.btnAsync.TabIndex = 3;
+            this.btnAsync.Text = "GetAsync";
+            this.btnAsync.UseVisualStyleBackColor = true;
+            this.btnAsync.Click += new System.EventHandler(this.btnAsync_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAsync);
+            this.Controls.Add(this.btnSync);
+            this.Controls.Add(this.txtRequerst);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtRequerst;
+        private System.Windows.Forms.Button btnSync;
+        private System.Windows.Forms.Button btnAsync;
+    }
+}
+
---
+++ WinFormsWebClient/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsWebClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSinc_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+            string line = "";
+            StringBuilder builder = new StringBuilder();
+            using(Stream stream=client.OpenRead(txtUrl.Text) )
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                        builder.AppendLine(line);
+                }
+               
+            }
+            txtRequerst.Text = builder.ToString();
+        }
+        private async Task GetTask()
+        {WebClient client = new WebClient();
+            string line = "";
+            StringBuilder builder = new StringBuilder();
+            Uri uri = new Uri(txtUrl.Text);
+            using (Stream stream = await client.OpenReadTaskAsync(uri))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                        builder.AppendLine(line);
+                }
+
+            }
+            txtRequerst.Text = builder.ToString();
+
+        }
+        private async void  btnAsync_Click(object sender, EventArgs e)
+        {
+            await GetTask();
+        }
+    }
+}
